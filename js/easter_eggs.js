9c850690af010082e71132d739dd94f2666a7c5a
DeveloperDemetri <developerdemetri@gmail.com>
2016-03-31 23:29:42 -0700

    fixed up site, added google analytics, and fixed pokemon easter egg

--- js/easter_eggs.js
+++ js/easter_eggs.js
@@ -3,9 +3,9 @@ var keys = [];
 var konami  = '38,38,40,40,37,39,37,39,66,65'; //up up down down left right left right b a
 var konami_url = "http://www.8bbit.com/play/contra/1125";
 var oot = '79,79,84,37,38,39,37,38,39'; //o o t left up right left up right
-var oot_url = 'fun/zeldas_lullaby.mp3'; //NOTE: I DO NOT OWN THIS SONG!!!
-var pika = '80,73,75,65,73,67,72,79,79,83,69,89,79,85'; //'pikachuichooseyou'
-var pika_url = "https://www.youtube.com/watch?v=MHqI-vwh83o";
+var oot_url = 'fun/zeldas_lullaby.mp3'; // I DO NOT OWN THIS SONG!!!
+var pika= '80,73,75,65,67,72,85,73,67,72,79,79,83,69,89,79,85'//'pikachuichooseyou'
+var pika_url = "https://www.youtube.com/watch?v=_CvBNRxpRqU";
 var entered = "";
 
 $(document).keydown(
@@ -14,7 +14,7 @@ $(document).keydown(
             entered = keys.toString();
             if ( entered.indexOf( konami ) >= 0 ) {
                 console.log("Konami activated");
-                $(location).attr('href', konami_url);
+                window.open(konami_url, '_blank');
                 keys = [];
             }
             if ( entered.indexOf( oot ) >= 0 ) { // thank you http://stackoverflow.com/a/8489802/5702582 //
@@ -27,7 +27,7 @@ $(document).keydown(
             }
             if ( entered.indexOf( pika ) >= 0 ) {
                 console.log("Pika activated");
-                $(location).attr('href', pika_url);
+                window.open(pika_url, '_blank');
                 keys = [];
             }
         }
