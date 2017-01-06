23ced4b3b24032f416a2a397fe8c1640f2e5c728
DeveloperDemetri <developerdemetri@gmail.com>
2017-01-05 22:01:29 -0700

    updated js files to use const and let

--- public/js/easter_eggs.js
+++ public/js/easter_eggs.js
@@ -1,34 +1,36 @@
 //thank you http://www.yourinspirationweb.com/en/fun-with-javascript-jquery-and-konami-code/ //
-var keys = [];
-var konami  = '38,38,40,40,37,39,37,39,66,65'; //up up down down left right left right b a
-var konami_url = "http://www.8bbit.com/play/contra/1125";
-var oot = '79,79,84,37,38,39,37,38,39'; //o o t left up right left up right
-var oot_url = 'fun/zeldas_lullaby.mp3'; // I DO NOT OWN THIS SONG!!!
-var pika= '80,73,75,65,67,72,85,73,67,72,79,79,83,69,89,79,85'//'pikachuichooseyou'
-var pika_url = "https://www.youtube.com/watch?v=_CvBNRxpRqU";
-var entered = "";
+const SERVER = location.protocol + '//' + location.hostname + ':' + location.port;
+const KONAMI  = '38,38,40,40,37,39,37,39,66,65'; //up up down down left right left right b a
+const KONAMI_URL = "http://www.8bbit.com/play/contra/1125";
+const OOT = '79,79,84,37,38,39,37,38,39'; //o o t left up right left up right
+const OOT_URL = SERVER+'fun/zeldas_lullaby.mp3'; // I DO NOT OWN THIS SONG!!!
+const PIKA = '80,73,75,65,67,72,85,73,67,72,79,79,83,69,89,79,85'//'pikachuichooseyou'
+const PIKA_URL = "https://www.youtube.com/watch?v=_CvBNRxpRqU";
+
+let entered = "";
+let keys = [];
 
 $(document).keydown(
-        function(e) {
-            keys.push( e.keyCode );
-            entered = keys.toString();
-            if ( entered.indexOf( konami ) >= 0 ) {
-                console.log("Konami activated");
-                window.open(konami_url, '_blank');
-                keys = [];
-            }
-            if ( entered.indexOf( oot ) >= 0 ) { // thank you http://stackoverflow.com/a/8489802/5702582 //
-                console.log("OOT activated");
-                var audioElement = document.createElement('audio');
-                audioElement.setAttribute('src', oot_url);
-                audioElement.setAttribute('autoplay', 'autoplay');
-                audioElement.play();
-                keys = [];
-            }
-            if ( entered.indexOf( pika ) >= 0 ) {
-                console.log("Pika activated");
-                window.open(pika_url, '_blank');
-                keys = [];
-            }
-        }
+  function(e) {
+      keys.push(e.keyCode);
+      entered = keys.toString();
+      if (entered.indexOf(KONAMI) >= 0) {
+          console.log("Konami activated");
+          window.open(KONAMI_URL, '_blank');
+          keys = [];
+      }
+      if (entered.indexOf(OOT) >= 0) { // thank you http://stackoverflow.com/a/8489802/5702582 //
+          console.log("OOT activated");
+          let audioElement = document.createElement('audio');
+          audioElement.setAttribute('src', OOT_URL);
+          audioElement.setAttribute('autoplay', 'autoplay');
+          audioElement.play();
+          keys = [];
+      }
+      if (entered.indexOf(PIKA) >= 0) {
+          console.log("Pika activated");
+          window.open(PIKA_URL, '_blank');
+          keys = [];
+      }
+  }
 );
