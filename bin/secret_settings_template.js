73dd2b419741a50ffbb7c7efb6114c932d429e55
DeveloperDemetri <developerdemetri@gmail.com>
2017-06-19 20:18:46 -0700

    fixed ssl issue on local dev

--- bin/secret_settings_template.js
+++ bin/secret_settings_template.js
@@ -2,6 +2,7 @@
 
 const SECRET_SETTINGS = {
   PORT: null, // server port
+  USE_SSL: false, // true if using SSL
   KEY_PATH: null, // path to SSL private key
   CERTIFICATE_PATH: null, // path to SSL certificate
   CA_PATH: null // path to CA certificate
